010e32c26f8296374439d24c03893fe68fb26f2c
AlejoGDOfficial <alejogdoff@gmail.com>
2026-06-04 18:09:20 -0500

    basic json ig

--- ale/json/JsonParser.hx
+++ ale/json/JsonParser.hx
@@ -37,22 +37,143 @@ class JsonParser
         return char;
     }
 
-    function advanceString():String
-        return String.fromCharCode(advance());
-    
     function isDigitStart(char:Int):Bool
         return char >= '0'.code && char <= '9'.code;
 
     function isDigit(char:Int):Bool
         return isDigitStart(char) || char == '.'.code;
     
-    function error()
-        throw 'Line: ' + line + ' - Column: ' + column;
+    function expect(char:Int)
+        if (peek() != char)
+            unexpected(char);
+        else
+            advance();
+
+    function unexpected(?expected:Int)
+        error('Unexpected Token: ' + peekString() + (expect == null ? '' : ' - Expected: ' + String.fromCharCode(expected)));
+
+    function error(msg:String)
+        throw 'Line: ' + line + ' - Column: ' + column + ': ' + msg;
+
+    function conditionalRead(func:Int -> Bool):String
+    {
+        var res:StringBuf = new StringBuf();
+
+        while (true)
+        {
+            final cur:Int = peek();
+
+            if (func(cur))
+                res.addChar(advance());
+            else
+                break;
+        }
+
+        return res.toString();
+    }
+
+    function clearSpaces()
+    {
+        while (true)
+        {
+            final cur = peek();
+
+            if (' '.code == cur || '\t'.code == cur || '\n'.code == cur || '\r'.code == cur)
+                advance()
+            else
+                break;
+        }
+    }
 
     public function parse():Dynamic
     {
-        var result = switch (peek())
+        clearSpaces();
+
+        final result:Dynamic = switch (peek())
         {
+            case '"'.code:
+                advance();
+
+                final res:String = conditionalRead(val -> val != '"'.code);
+
+                advance();
+
+                res;
+
+            case '['.code:
+                final res:Array<Dynamic> = [];
+
+                advance();
+
+                var shouldContinue:Bool = peek() != ']'.code;
+
+                while (shouldContinue)
+                {
+                    res.push(parse());
+
+                    shouldContinue = switch (peek())
+                    {
+                        case ','.code:
+                            advance();
+
+                            true;
+                            
+                        case ']'.code:
+                            false;
+
+                        default:
+                            unexpected(']'.code);
+
+                            false;
+                    }
+                }
+
+                advance();
+
+                res;
+
+            case '{'.code:
+                final res:Dynamic = {};
+
+                advance();
+
+                var shouldContinue:Bool = peek() != '}'.code;
+
+                clearSpaces();
+
+                while (shouldContinue)
+                {
+                    expect('"'.code);
+
+                    final fieldName:String = conditionalRead(val -> val != '"'.code);
+
+                    advance();
+
+                    expect(':'.code);
+
+                    Reflect.setField(res, fieldName, parse());
+
+                    shouldContinue = switch (peek())
+                    {
+                        case ','.code:
+                            advance();
+
+                            true;
+
+                        case '}'.code:
+                            false;
+
+                        default:
+                            unexpected('}'.code);
+
+                            false;
+                    }
+                }
+
+                advance();
+
+                res;
+
             default:
                 if (isDigitStart(peek()))
                     Std.parseFloat(conditionalRead(isDigit))
@@ -60,16 +181,8 @@ class JsonParser
                     null;
         }
 
+        clearSpaces();
+
         return result;
-    }
-
-    function conditionalRead(func:Int -> Bool)
-    {
-        var res:String = '';
-
-        while (func(peek()))
-            res += advanceString();
-
-        return res;
     }
 }
--- tests/Main.hx
+++ tests/Main.hx
@@ -1,13 +1,31 @@
 package;
 
+import haxe.Timer;
+
 import ale.json.*;
 
 class Main
 {
     static function main()
     {
+        function bechmark(func:Void -> Void, ?title:String)
+        {
+            final start:Float = Timer.stamp();
+
+            for (i in 0...10000)
+                func();
+
+            trace((title == null ? '' : title + ' Json : ') + Std.string(Timer.stamp() - start));
+        }
+
         Config.PATH = 'data/';
 
-        trace(Json.parse('test') is Float);
+        final source = Config.FILE_READER(Config.PATH + 'test' + Config.EXTENSION);
+
+        bechmark(() -> Json.parse(source), 'ALE');
+
+        bechmark(() -> haxe.Json.parse(source), 'Std');
+
+        bechmark(() -> jsonmod.Json.parse(source), 'Mod');
     }
 }
